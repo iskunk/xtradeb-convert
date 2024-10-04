31408e639e43f45f63ea016474541c1b24b5b669
Daniel Richard G <skunk@iSKUNK.ORG>
2024-10-04 02:19:24 -0400

    thunderbird: Ready to fly
    The conversion script now works on either the stable (115.15.0) or
    latest (128.2.3esr) version of Thunderbird.
    
    xtradeb_fortify-source-3-v115.patch: Copy/rename of the existing
    patch, needed for the stable version
    
    xtradeb_fortify-source-3.patch: Updated to apply to the latest version
    
    xtradeb_mach-python-312.patch: New patch to address scripts not liking
    an up-to-date version of Python
    
    xtradeb_skia-cpu-arch.patch: New patch to fix a silly oversight
    
    thunderbird.sh:
    
    * Indicate support for the bookworm (stable) version of the package
    
    * Add a variable for control of stable vs. latest logic
    
    * The librnp-dev downgrade is no longer necessary, as the edit was
      applied by Debian
    
    * Use cargo/rustc 1.76 (but on oracular, use 1.80)
    
    * On noble, the latest version cannot build with the system libnss
    
    * Disable the renaming of a Python package directory to fix the build
    
    * Make the backend.mk edit cover the files for both stable and latest,
      and document which is which
    
    * Apply the appropriate fortify-source-3 patch for stable vs. latest
    
    * Apply mach-python-312.patch and skia-cpu-arch.patch to the latest
      version

--- pkg/thunderbird.sh
+++ pkg/thunderbird.sh
@@ -3,6 +3,7 @@
 #
 # This script operates on the debian/ subdirectory of a Debian
 # thunderbird source package, as available from
+# https://packages.debian.org/source/bookworm/thunderbird#pdownload
 # https://packages.debian.org/source/sid/thunderbird#pdownload
 #
 
@@ -27,6 +28,11 @@ then
 	exit 1
 fi
 
+stable=no
+case "$deb_version" in
+	?:115.*) stable=yes ;;
+esac
+
 ################################################################
 ##
 ## Modifications to allow building on Ubuntu noble and later
@@ -40,9 +46,6 @@ sed -i -r \
 	-e '/^\s+libotr5,/s/,/t64,/' \
 	$debian/control
 
-# Downgrade this dependency slightly to accommodate noble
-sed -i -r '/^\s+librnp-dev /s/ 0\.17\.1\b/ 0.17.0/' $debian/control
-
 # Use Clang/LLVM 17 specifically
 # (the build fails mysteriously in a Rust component with clang-18)
 sed -i -r \
@@ -51,18 +54,50 @@ sed -i -r \
 	-e 's/^(\s+llvm)-dev,/\1-17-dev,/' \
 	$debian/control
 
+# Use version-specific cargo/rustc packages
+sed -i -r 's/^(\s+(cargo|rustc)) \(.+\),/\1-1.76,/' $debian/control
+
 cat >>$debian/rules <<'END'
 
 # XtraDeb additions
 
 export LLVM_OBJDUMP = llvm-objdump-17
 export MOZ_LIBCLANG_PATH = /usr/lib/llvm-17/lib
+
+export CARGO = cargo-1.76
+export RUSTC = rustc-1.76
 END
+
+# Oracular does not have cargo/rustc 1.76, and 1.74 is too old
+if ubuntu_dist oracular
+then
+	sed -i -r 's/\b(cargo|rustc)-1.76\b/\1-1.80/' \
+		$debian/control \
+		$debian/rules
+fi
+
+if [ $stable = no ] && ubuntu_dist noble
+then
+	# Use the bundled libnss as the system one is too old
+	sed -i '/^ac_add_options --with-system-nss/s/with/without/' \
+		$debian/mozconfig.default
+fi
 
 # Missing build dependency, not needed on Debian for some reason
 # "pynacl 1.5.0 requires cffi, which is not installed."
 perl -pi -e '/^(\s+)python3,/ and $_ .= "${1}python3-cffi,\n"' \
 	$debian/control
+
+# Don't rename the typing_extensions directory, as that breaks the build:
+#
+#   [...]
+#     File ".../third_party/python/fluent.syntax/fluent/syntax/stream.py", line 2, in <module>
+#       from typing_extensions import Literal
+#   ModuleNotFoundError: No module named 'typing_extensions'
+#   make[5]: *** [backend.mk:636: toolkit/content/neterror/.deps/aboutNetErrorCodes.js.stub] Error 1
+#
+perl -pi -e 'm!^\s+mv third_party/python/typing_extensions! and s/(mv)/#XtraDeb#$1/' \
+	$debian/rules
 
 #### Fixes for LTO-enabled build
 ##
@@ -71,12 +106,16 @@ perl -pi -e '/^(\s+)python3,/ and $_ .= "${1}python3-cffi,\n"' \
 
 # Find the right file to modify with e.g.
 #   find obj-thunderbird -name \*.mk -exec grep -l RUST_LIBRARY_FEATURES {} +
+# Note:
+#   .../toolkit/library/rust/backend.mk is for Thunderbird 115
+#   .../comm/rust/gkrust/backend.mk is for Thunderbird >= 128
 perl -pi \
 	-e '/mach -v configure/ and $_ .= <<END;' \
 	-e '' \
 	-e '	# XtraDeb: workaround for LTO breakage in webrender build' \
 	-e '	perl -pi -e \x{27}s/-flto(=\\w+)?//g; s/-ffat-lto-objects//g\x{27} \\' \
-	-e '		obj-thunderbird/toolkit/library/rust/backend.mk' \
+	-e '		obj-thunderbird/toolkit/library/rust/backend.mk \\' \
+	-e '		obj-thunderbird/comm/rust/gkrust/backend.mk' \
 	-e 'END' \
 	$debian/rules
 
@@ -86,7 +125,15 @@ new_patch xtradeb/fix-param-lto-partitions.patch
 ## Patch series modifications
 ##
 
-new_patch xtradeb/fortify-source-3.patch
+tag=$(test $stable = no || echo -v115)
+
+new_patch xtradeb/fortify-source-3$tag.patch
+
+if [ $stable = no ]
+then
+	new_patch xtradeb/mach-python-312.patch
+	new_patch xtradeb/skia-cpu-arch.patch
+fi
 
 ################################################################
 
