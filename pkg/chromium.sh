eee64989ef1fee41fd936be44f136e6e39e7d557
Daniel Richard G <skunk@iSKUNK.ORG>
2024-11-04 04:29:03 -0500

    chromium: Update for 130.0.6723.91-2
    xtradeb_timeval-cast.patch: Drop, as Debian now has their own patch
    for this issue
    
    chromium.sh: Drop timeval-cast.patch

--- pkg/chromium.sh
+++ pkg/chromium.sh
@@ -208,8 +208,6 @@ then
 	new_patch xtradeb/openjpeg-no-strict-mode.patch
 fi
 
-new_patch xtradeb/timeval-cast.patch
-
 new_patch xtradeb/warning-fixes.patch
 
 ################################################################
