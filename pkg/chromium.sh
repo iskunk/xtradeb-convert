8c295e0890ce307521136ffdb385938a2a690f1d
Daniel Richard G <skunk@iSKUNK.ORG>
2025-02-18 04:28:54 -0500

    chromium: Work around another dependency oversight
    This compile error occurs in the ungoogled-chromium build, which is
    configured with safe_browsing_mode=0:
    
    FAILED: obj/chrome/browser/ui/views/download/download/download_bubble_contents_view.o
    [...]
    In file included from ../../chrome/browser/ui/views/download/bubble/download_bubble_contents_view.cc:5:
    In file included from ../../chrome/browser/ui/views/download/bubble/download_bubble_contents_view.h:10:
    ../../chrome/browser/download/download_ui_model.h:23:10: fatal error: 'components/safe_browsing/content/common/proto/download_file_types.pb.h' file not found
       23 | #include "components/safe_browsing/content/common/proto/download_file_types.pb.h"
          |          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    1 error generated.
    
    Reported upstream at https://issues.chromium.org/397258344 .

--- pkg/chromium.sh
+++ pkg/chromium.sh
@@ -66,7 +66,9 @@ cp -fp $base_dir/_chromium/keepalive-wrapper.py $debian/scripts/
 perl -pi \
 	-e '/ninja .+ chrome/ and $_= <<END . $_;' \
 	-e '	# XtraDeb workaround for https://crbug.com/40943790' \
-	-e '	ninja -j\$(njobs) -C out/Release ui/webui/resources/cr_components/history_clusters:build_ts' \
+	-e '	# and https://issues.chromium.org/397258344' \
+	-e '	ninja -j\$(njobs) -C out/Release ui/webui/resources/cr_components/history_clusters:build_ts \\' \
+	-e '		obj/components/safe_browsing/content/common/proto/download_file_types_proto_gen.stamp' \
 	-e 'END' \
 	$debian/rules
 
