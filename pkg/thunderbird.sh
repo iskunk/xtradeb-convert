81cac3dade4777a62a0f74986f0b5e3b945eb212
Daniel Richard G <skunk@iSKUNK.ORG>
2025-04-16 01:55:14 -0400

    thunderbird: General revamp and support for plucky
    xtradeb_fortify-source-3-v115.patch: Drop, as 115 is no longer the
    stable release version
    
    thunderbird.sh: Various updates:
    
    * Drop recognition of 115 as the stable version, as well as the stable
      version variable, since there is no longer a significant version gap
      between stable and unstable
    
    * Update bookworm package tweaks
    
    * Drop the use of LLVM 17, as the mysterious build failure with 18 no
      longer occurs
    
    * Use lld for a faster final link
    
    * Disable full LTO, as it need too many resources, and enable thin LTO
    
    * Update Rust logic to use 1.84 (on plucky) or 1.80 (everywhere else)
    
    * Drop python3-cffi tweak as it is not needed (the issue is just a
      warning)
    
    * Drop typing_extensions edit as it is no longer needed
    
    * Drop webrender LTO fix as it is no longer needed
    
    * Consolidate patch series additions

--- pkg/thunderbird.sh
+++ pkg/thunderbird.sh
@@ -22,11 +22,6 @@ grep -Fqx 'Source: thunderbird' $debian/control 2>/dev/null \
 ! ubuntu_dist jammy \
 || not_applicable 'release already has an official thunderbird package'
 
-stable=no
-case "$deb_version" in
-	?:115.*) stable=yes ;;
-esac
-
 ################################################################
 ##
 ## Modifications to allow building on Ubuntu noble and later
@@ -35,99 +30,59 @@ esac
 
 # Some tweaks particular to the ~deb12 (bookworm) package
 sed -i -r \
-	-e '/^\s+autoconf2\.13,/d' \
-	-e '/^\s+libfontconfig1-dev,/s/1//' \
-	-e '/^\s+libotr5,/s/,/t64,/' \
+	-e '/^\s+(cargo|cbindgen|rustc)-web /s/-web//' \
+	-e '/^\s+libotr5,$/s/,/t64,/' \
 	$debian/control
 
-# Use Clang/LLVM 17 specifically
-# (the build fails mysteriously in a Rust component with clang-18)
-sed -i -r \
-	-e 's/^(\s+clang),/\1-17,/;' \
-	-e 's/^(\s+libclang)-dev,/\1-17-dev,/;' \
-	-e 's/^(\s+llvm)-dev,/\1-17-dev,/' \
-	$debian/control
+# Use lld for a faster final link
+perl -pi -e '/^(\s+)clang,$/ and $_.="${1}lld,\n"' $debian/control
+sed -i '/-Wl,--reduce-memory-overheads/s/^/#xtradeb#/' $debian/rules
+
+cat >$debian/xtradeb.tmp <<END
+
+# XtraDeb: Disable default LTO options, as they make for an expensive build
+export DEB_BUILD_MAINT_OPTIONS += optimize=-lto
+END
+(cd $debian && sed -i '/export DH_VERBOSE=1/ r xtradeb.tmp' rules)
+rm $debian/xtradeb.tmp
+
+cat >$debian/xtradeb.tmp <<END
+
+# XtraDeb specific things
+ac_add_options --enable-lto=thin
+END
+(cd $debian && sed -i '/--enable-application=comm\/mail/ r xtradeb.tmp' \
+	mozconfig.thunderbird)
+rm $debian/xtradeb.tmp
+
+rust_version=$(ubuntu_dist plucky && echo 1.84 || echo 1.80)
 
 # Use version-specific cargo/rustc packages
-sed -i -r 's/^(\s+(cargo|rustc)) \(.+\),/\1-1.76,/' $debian/control
+sed -i -r 's/^(\s+(cargo|rustc)) \(.+\),/\1-'"$rust_version,/" $debian/control
 
-cat >>$debian/rules <<'END'
+cat >>$debian/rules <<END
 
 # XtraDeb additions
 
-export LLVM_OBJDUMP = llvm-objdump-17
-export MOZ_LIBCLANG_PATH = /usr/lib/llvm-17/lib
-
-export CARGO = cargo-1.76
-export RUSTC = rustc-1.76
+export CARGO = cargo-$rust_version
+export RUSTC = rustc-$rust_version
 END
 
-# Oracular does not have cargo/rustc 1.76, and 1.74 is too old
-if ubuntu_dist oracular
-then
-	sed -i -r 's/\b(cargo|rustc)-1.76\b/\1-1.80/' \
-		$debian/control \
-		$debian/rules
-fi
-
-if [ $stable = no ] && ubuntu_dist noble
+if ubuntu_dist noble
 then
 	# Use the bundled libnss as the system one is too old
 	sed -i '/^ac_add_options --with-system-nss/s/with/without/' \
 		$debian/mozconfig.default
 fi
 
-# Missing build dependency, not needed on Debian for some reason
-# "pynacl 1.5.0 requires cffi, which is not installed."
-perl -pi -e '/^(\s+)python3,/ and $_ .= "${1}python3-cffi,\n"' \
-	$debian/control
-
-# Don't rename the typing_extensions directory, as that breaks the build:
-#
-#   [...]
-#     File ".../third_party/python/fluent.syntax/fluent/syntax/stream.py", line 2, in <module>
-#       from typing_extensions import Literal
-#   ModuleNotFoundError: No module named 'typing_extensions'
-#   make[5]: *** [backend.mk:636: toolkit/content/neterror/.deps/aboutNetErrorCodes.js.stub] Error 1
-#
-perl -pi -e 'm!^\s+mv third_party/python/typing_extensions! and s/(mv)/#XtraDeb#$1/' \
-	$debian/rules
-
-#### Fixes for LTO-enabled build
-##
-## Same issues as with firefox: https://bugs.debian.org/1050890
-##
-
-# Find the right file to modify with e.g.
-#   find obj-thunderbird -name \*.mk -exec grep -l RUST_LIBRARY_FEATURES {} +
-# Note:
-#   .../toolkit/library/rust/backend.mk is for Thunderbird 115
-#   .../comm/rust/gkrust/backend.mk is for Thunderbird >= 128
-perl -pi \
-	-e '/mach -v configure/ and $_ .= <<END;' \
-	-e '' \
-	-e '	# XtraDeb: workaround for LTO breakage in webrender build' \
-	-e '	perl -pi -e \x{27}s/-flto(=\\w+)?//g; s/-ffat-lto-objects//g\x{27} \\' \
-	-e '		obj-thunderbird/toolkit/library/rust/backend.mk \\' \
-	-e '		obj-thunderbird/comm/rust/gkrust/backend.mk' \
-	-e 'END' \
-	$debian/rules
-
-new_patch xtradeb/fix-param-lto-partitions.patch
-
 ##
 ## Patch series modifications
 ##
 
-tag=$(test $stable = no || echo -v115)
-
-new_patch xtradeb/fortify-source-3$tag.patch
-
-if [ $stable = no ]
-then
-	new_patch xtradeb/mach-python-312.patch
-	new_patch xtradeb/skia-cpu-arch.patch
-fi
+new_patch xtradeb/fix-param-lto-partitions.patch
+new_patch xtradeb/fortify-source-3.patch
+new_patch xtradeb/mach-python-312.patch
+new_patch xtradeb/skia-cpu-arch.patch
 
 ################################################################
 
