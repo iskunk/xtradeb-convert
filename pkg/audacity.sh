b78226c09b177b132297ce030642e4602fcc43b4
Daniel Richard G. <skunk@iSKUNK.ORG>
2025-10-30 19:28:06 -0400

    audacity: Update for 3.7.5
    XtraDeb-defuse-wxwidgets-lib-check.patch: New patch to loosen up an
    overly thorough wxWidgets dependency library check, so that CMake can
    actually find and use wxWidgets
    
    xtradeb_libsbsms-static.patch: Drop, as we now build without libsbsms
    on jammy
    
    audacity.sh: Add some new required edits, including disabling sbsms
    and using a static build of wxWidgets, and change out the applied
    patch

--- pkg/audacity.sh
+++ pkg/audacity.sh
@@ -28,9 +28,24 @@ ubuntu_dist jammy || not_applicable
 
 if ubuntu_dist jammy
 then
-	# Bump down dependency version slightly
-	perl -pi -e '/\blibmp3lame-dev \(>= .+\),/ and s/3.100-5/3.100-3/;' \
+	# Bump down this dependency version slightly
+	sed -ri '/^\s+libmp3lame-dev/ s/3.100-5/3.100-3/' \
 		$debian/control
+
+	# Not available
+	sed -ri \
+		-e '/^\s+libsbsms-dev[ ,]/d' \
+		-e '/^\s+libvst3sdk-dev,/d' \
+		$debian/control
+
+	# Don't go looking for libsbsms
+	perl -pi \
+		-e '/-Daudacity_use_ffmpeg=/' \
+		-e 'and $_ .= "\t  -Daudacity_use_sbsms=off \\\n";' \
+		$debian/rules
+
+	# Use the static config of wxWidgets
+	sed -ri 's/(gtk3-unicode)-3/\1-static-3/' $debian/rules
 fi
 
 ##
@@ -39,7 +54,7 @@ fi
 
 if ubuntu_dist jammy
 then
-	new_patch xtradeb/libsbsms-static.patch
+	new_patch XtraDeb-defuse-wxwidgets-lib-check.patch
 fi
 
 ################################################################
