d79d940b5853122673aa7a39bf6a16a62d1f436d
Daniel Richard G. <skunk@iSKUNK.ORG>
2026-03-04 17:37:49 -0500

    thunderbird: Update for 153.0.1esr, prepare for Betterbird
    **** TODO: UPDATE REMAINDER OF COMMIT MESSAGE ****
    
    functions.sh: Map a "betterbird" package to use the thunderbird
    conversion
    
    thunderbird/script.sh: Various updates:
    
    * Allow converting a non-thunderbird (betterbird) package on jammy
    
    * Override LLVM version on jammy to use the officially-available 15
      (builds just fine with it)
    
    * Drop the bookworm conversion code, as it's no longer useful
    
    * Use version-specific LLVM build-deps (e.g. clang-20 instead of
      plain clang)
    
    * Specify CARGO and RUSTC in the mozconfig instead of setting
      environment variables in d/rules; also specify CC and CXX
    
    * Downgrade the libgpgme45 binary package dependency for releases
      prior to resolute, due to it not being available before then
    
    * Drop the libnss3-dev build-dep when we're using the bundled libnss,
      and enable a lintian override for the resulting embedded library
    
    * Downgrade the librnp* package dependencies on jammy
    
    * Use a keepalive wrapper for the build to avoid timeouts on Launchpad
    
    * Use nice(1) on Launchpad to make the build more tractable for the
      build host (hopefully)
    
    * Don't apply fortify-source-3.patch on jammy
    
    * Apply libyuv-rvv-support.patch for the newer releases that target
      RVA23 on riscv64
    
    * Drop skia-cpu-arch.patch, add python-314-update.patch
    
    * Only bump the version epoch for thunderbird proper
    
    xtradeb_fortify-source-3.patch: Refresh
    
    xtradeb_libyuv-rvv-support.patch: New patch to fix riscv64 link errors
    due to necessary source files not being properly listed (?!)
    
    xtradeb_python-314-update.patch: New patch to fix Python build
    infrastructure breakage on resolute
    
    xtradeb_skia-cpu-arch.patch: Drop, no longer needed

--- pkg/thunderbird/script.sh
+++ pkg/thunderbird/script.sh
@@ -7,28 +7,25 @@
 
 xd_convert() {
 
-! ubuntu_dist jammy \
+test "_$source_name" != _thunderbird \
+|| ! ubuntu_dist jammy \
+|| test -n "${XTRADEB_NON_PRODUCTION:-}" \
 || not_applicable 'release already has an official thunderbird package'
 
-################################################################
-##
-## Modifications to allow building on Ubuntu noble and later
-##
-################################################################
-
-# Some tweaks particular to the ~deb12 (bookworm) package
-sed -i -r \
-	-e '/^\s+(cargo|cbindgen|rustc)-web /s/-web//' \
-	-e '/^\s+libotr5,$/s/,/t64,/' \
+# Use version-specific LLVM packages
+sed -ri \
+	-e 's/^(\s+clang),/\1-'"$llvm_version"',/' \
+	-e 's/^(\s+(libclang|llvm))(-dev),/\1-'"$llvm_version"'\3,/' \
 	$debian/control
 
 # Use lld for a faster final link
 # (this and the "optimize=-lto" bit are submitted upstream at
 # https://salsa.debian.org/mozilla-team/thunderbird/-/merge_requests/10)
-perl -pi -e '/^(\s+)clang,$/ and $_.="${1}lld,\n"' $debian/control
-sed -i '/-Wl,--reduce-memory-overheads/s/^/#xtradeb#/' $debian/rules
+perl -pi -e '/^(\s+)clang-\d+,$/ and $_.="${1}lld-'"$llvm_version"',\n"' \
+	$debian/control
+sed -i '/-Wl,--reduce-memory-overheads/ s/^/#xtradeb#/' $debian/rules
 
-cat >$debian/xtradeb.tmp <<END
+cat > $debian/xtradeb.tmp << END
 
 # XtraDeb: Disable default LTO options, as they make for an expensive build
 export DEB_BUILD_MAINT_OPTIONS += optimize=-lto
@@ -36,43 +33,81 @@ END
 (cd $debian && sed -i '/export DH_VERBOSE=1/ r xtradeb.tmp' rules)
 rm $debian/xtradeb.tmp
 
-cat >$debian/xtradeb.tmp <<END
+# Use version-specific cargo/rustc packages
+sed -ri 's/^(\s+(cargo|rustc)) \([^,]+\),/\1-'"$rust_version,/" \
+	$debian/control
+
+cat > $debian/xtradeb.tmp << END
 
 # XtraDeb specific things
-ac_add_options --enable-lto=thin
+ac_add_options CC=clang-$llvm_version
+ac_add_options CXX=clang++-$llvm_version
+ac_add_options CARGO=cargo-$rust_version
+ac_add_options RUSTC=rustc-$rust_version
+
+# On riscv64, LTO yields "relocation R_RISCV_JAL out of range" link errors
+if [ "_\$DEB_HOST_ARCH" != _riscv64 ]; then
+  ac_add_options --enable-lto=thin
+fi
 END
 (cd $debian && sed -i '/--enable-application=comm\/mail/ r xtradeb.tmp' \
 	mozconfig.thunderbird)
 rm $debian/xtradeb.tmp
 
-# Use version-specific cargo/rustc packages
-sed -i -r 's/^(\s+(cargo|rustc)) \(.+\),/\1-'"$rust_version,/" $debian/control
+# Fix the format of a couple commented-out lintian overrides
+# (as we might need to use them)
+sed -ri 's!^(#\w+bird: embedded-library) (usr/lib/\S+): (\w+)$!\1 \3 [\2]!' \
+	$debian/*.lintian-overrides
 
-cat >>$debian/rules <<END
-
-# XtraDeb additions
-
-export CARGO = cargo-$rust_version
-export RUSTC = rustc-$rust_version
-END
-
-if ubuntu_dist noble
+if ubuntu_dist jammy noble resolute
 then
 	# Use the bundled libnss as the system one is too old
-	sed -i '/^ac_add_options --with-system-nss/s/with/without/' \
+	sed -ri '/^\s+libnss3-dev / d' $debian/control
+	sed -i '/^ac_add_options --with-system-nss/ s/with/without/' \
 		$debian/mozconfig.default
+	sed -ri '/^#\w+bird: embedded-library nss / s/^#//' \
+		$debian/*.lintian-overrides
 fi
+
+if ubuntu_dist jammy
+then
+	# No ...t64 form available
+	sed -ri 's/^(\s+libotr5)t64,/\1,/' $debian/control
+
+	# Downgrade this dependency
+	sed -ri '/^\s+librnp(-dev|0) / s/0\.17\.0/0.15.2/' $debian/control
+fi
+
+# Use a keepalive wrapper, as some link operations take a long time,
+# and nice(1) the build down when running on Launchpad to avoid spurious
+# (and log-less) "Failed to build" outcomes
+cp $base_dir/pkg/chromium/keepalive-wrapper.py $debian/
+sed -ri \
+	-e '/^\s+dh_auto_build/ i \	debian/keepalive-wrapper.py 9999 \\' \
+	$debian/rules
 
 ##
 ## Patch series modifications
 ##
 
 new_patch xtradeb/fix-param-lto-partitions.patch
-new_patch xtradeb/fortify-source-3.patch
-new_patch xtradeb/mach-python-312.patch
-new_patch xtradeb/skia-cpu-arch.patch
 
-need_version_epoch_bump=yes
+if ! ubuntu_dist jammy
+then
+	new_patch xtradeb/fortify-source-3.patch
+fi
+
+if ! ubuntu_dist jammy noble
+then
+	new_patch xtradeb/libyuv-rvv-support.patch
+fi
+
+new_patch xtradeb/python-no-pip-check.patch
+
+if [ "_$source_name" = _thunderbird ]
+then
+	need_version_epoch_bump=yes
+fi
 
 } # xd_convert()
 
