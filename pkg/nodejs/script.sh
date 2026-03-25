a24abcab3c157ecc92a85261ecde87f5d4536d63
Daniel Richard G. <skunk@iSKUNK.ORG>
2026-03-25 18:43:51 -0400

    nodejs: Update for version 20
    * Node.js v20 is an upgrade for jammy and noble
    
    * Drop modifications to node-cjs-module-lexer, node-undici, and
      pkg-js-tools, as these are usable as-is from the archive
    
    * Adjust build/package dependencies
    
    * Build static libraries only
    
    * Use the bundled libuv to avoid issues with the older system one
    
    * Don't build documentation (requires more dependencies)
    
    * Skip some failing tests
    
    * Reduce memory usage slightly on jammy to avoid some OOMs
    
    * Work around missing pkg-config file in libsimdjson-dev on jammy
    
    * Apply new patches
    
    xtradeb-gyp-subset.patch: New patch that brings in a few files from
    the "gyp" package needed to run the build
    
    xtradeb-node-cjs-module-lexer.patch: Drop, no longer needed
    
    xtradeb-node-undici.patch: Drop, no longer needed
    
    xtradeb-nodejs.patch: Drop, no longer needed now that we are using the
    bundled version of libuv
    
    xtradeb-nodejs20-c-ares-downgrade.patch: New patch to downgrade the
    c-ares API to what is available on jammy
    
    xtradeb-nodejs20-no-double-build.patch: New patch to avoid building
    everything a second time on "make install"

--- pkg/nodejs/script.sh
+++ pkg/nodejs/script.sh
@@ -5,58 +5,160 @@
 # https://packages.debian.org/source/sid/node-cjs-module-lexer#pdownload
 # https://packages.debian.org/source/sid/nodejs#pdownload
 #
+#
+# Note: To build nodejs v20 on jammy, the following packages need to be
+# imported from a newer release:
+#
+#   node-cjs-module-lexer
+#   node-llhttp
+#   node-minimatch
+#   node-undici
+#
+# On noble, only one is needed: node-minimatch
+#
+# To build chromium on jammy, the following additional packages need to be
+# imported from a newer release:
+#
+#   node-ampproject-remapping (a.k.a. node-jridgewell-source-map)
+#   node-glob
+#   node-rollup-plugin-terser
+#   node-terser
+#
+# (On noble, no additional packages are needed.)
+#
 
 ################################################################
 
 xd_convert() {
 
-ubuntu_dist jammy || not_applicable 'this script is needed only for jammy'
+ubuntu_dist jammy noble VENDOR \
+|| not_applicable 'this script is needed only for jammy and noble'
 
-case "$source_name" in
-	pkg-js-tools)
-	# Downgrade the node-marked-man dep to what is available in jammy
-	# (the package still builds just fine)
-	perl -pi -e '/^ , node-marked-man/ and s/\(>= .+?\)/(>= 0.7.0)/' \
-		$debian/control
-	echo 'Downgrade the node-marked-man build-dependency.' > $changelog_entry_file
-	;;
-
-	node-undici)
-	# Fix "error TS2307: Cannot find module 'ms' or its corresponding
-	# type declarations"
-	mkdir -p $debian/xtradeb-fix/ms
-	url=https://github.com/vercel/ms/raw/dfd18036f359a3b80bf38d3d8c0bb850c0dc9b66/src/index.ts
-	(cd $debian/xtradeb-fix/ms && wget -nv $url)
-	echo "Downloaded from $url" >$debian/xtradeb-fix/ms/README
-	new_patch xtradeb-node-undici.patch
-	# This step fails with a 'Could not resolve "streamsearch"' error
-	# when building packages from lunar or mantic, no idea why
-	perl -pi -e '/^esbuild/ and s/$/ || (: XtraDeb: Oh well, we tried && touch undici-fetch.js)/' \
-		$debian/nodejs/build
-	echo 'Add missing "ms" module, and work around esbuild failure.' > $changelog_entry_file
-	;;
-
-	node-cjs-module-lexer)
-	# Fix "Error: Cannot find module 'node:fs/promises'"
-	perl -pi -e '/^ , node-babel-plugin-transform-modules-commonjs/ and $_.=" , node-istanbul\n"' \
-		$debian/control
-	new_patch xtradeb-node-cjs-module-lexer.patch
-	echo 'Add node-istanbul build-dependency and use alternate implementation of "node:fs/promises".' > $changelog_entry_file
-	;;
-
-	nodejs)
-	perl -pi -e 'if(/^exp-relax-check :=/){s/^/#xtradeb#/; $_.="exp-relax-check = -i\n"}' \
-		$debian/rules
-	new_patch xtradeb-nodejs.patch
-	echo 'Fix unavailable uv_available_parallelism() call, and allow test suite failures.' > $changelog_entry_file
-	;;
-
-	*)
-	error "$pkg: not a supported package"
-	;;
+case "$deb_version" in
+	20.*) ;;
+	*) error 'only version nodejs 20.*.* is supported at this time' ;;
 esac
 
+add_to_changelog << END
+NOTE: This package has been modified to provide static libraries only, and
+no documentation. It is intended solely for use as a build dependency.
+END
+
+# Drop/downgrade some build dependencies
+sed -ri \
+	-e '/^\s+gyp / d' \
+	-e '/^\s+libnghttp3-dev / s/>= .*\)/>= 0.1.1)/' \
+	-e '/^\s+libngtcp2-dev / s/>= .*\)/>= 0.1.0)/' \
+	-e '/^\s+libsimdjson-dev / s/>= .*\)/>= 1.0.2)/' \
+	-e '/^\s+node-.* <!nodoc>,/ d' \
+	$debian/control
+
+# Upgrade these to reflect reality
+sed -ri \
+	-e '/^\s+libicu-dev / s/>= .*\)/>= 71.0)/' \
+	$debian/control
+
+if ubuntu_dist jammy
+then
+	# There is no pre-existing (older) package of nodejs for jammy on
+	# riscv64, which is a problem, as a number of the build dependencies
+	# require it (via Depends:, despite not actually *using* it).
+	# We hack APT with a dummy package to get past that annoyance.
+	perl -pi -e '/^(\s+)curl / and $_.="${1}dummy-nodejs [riscv64],\n"' \
+		$debian/control
+
+	# Pkg-config is better supported on jammy than pkgconf
+	sed -ri 's/^(\s+)pkgconf,/\1pkg-config,/' $debian/control
+fi
+
+# Drop this install dependency, as it does not appear to be needed
+# (and the package is not available in jammy/noble at any rate)
+sed -ri '/^\s+node-corepack / d' $debian/control
+
+# Copy over the node-* package dependencies from libnodeNNN to nodejs,
+# as we will be getting rid of the former, and need those dependencies
+# to take effect when the package is used.
+sed -nr '/^Package: libnode[0-9]{3}/,/^Description:/ p' $debian/control \
+| grep -E '^\s+node-' \
+> $debian/xtradeb.tmp
+(cd $debian && sed -ri -e '/^\s+libnode[0-9]{3} \([^)]+\)/ {r xtradeb.tmp' -e 'd}' control)
+rm $debian/xtradeb.tmp
+# Don't build the libnodeNNN package, nor its shared library
+# (for better build-dep hygiene)
+zap_control_package 'libnode\d{3}' $debian/control
+sed -i '/^\s*--shared \\$/ d' $debian/rules
+
+# Use the bundled libuv
+sed -ri '/^\s+libuv1-dev[ :]/ d' $debian/control
+sed -i '/^\s*DEB_CONFIGURE_EXTRA_FLAGS += --shared-libuv/ s/^/#xtradeb#/' \
+	$debian/rules
+
+# Don't build documentation, as this requires additional dependencies
+sed -ri '/\$\(MAKE\) doc-only/ s/^/#xtradeb#/' $debian/rules
+rm -v $debian/*.docs
+zap_control_package nodejs-doc $debian/control
+# Drop the Recommends:, too
+sed -ri '/^\s+nodejs-doc$/ d' $debian/control
+
+cat > $debian/xtradeb.tmp << END
+# XtraDeb
+# noble
+skipTests += parallel/test-eventsource.js
+skipTests += parallel/test-tls-alert.js
+skipTests += parallel/test-tls-session-cache.js
+# long timeouts on jammy + noble
+skipTests += parallel/test-cluster-primary-error.js
+skipTests += parallel/test-cluster-primary-kill.js
+
+END
+(cd $debian && sed -i -e '/^export HOME =/ {r xtradeb.tmp' -e 'N}' rules)
+rm $debian/xtradeb.tmp
+
+if ubuntu_dist jammy
+then
+	# Linking seems to need quite a bit more RAM on jammy
+	sed -i \
+		-e '/^export CPPFLAGS$/ {i # XtraDeb' \
+		-e 'i LDFLAGS += -Wl,--reduce-memory-overheads\n' \
+		-e '}' \
+		$debian/rules
+
+	mkdir $debian/pkgconfig
+	cat > $debian/pkgconfig/simdjson.pc << END
+# XtraDeb workaround for the lack of a simdjson.pc file in the
+# libsimdjson-dev 1.0.2 package in jammy
+
+Name: simdjson
+Cflags: -DSIMDJSON_THREADS_ENABLED=1
+Libs: -lsimdjson
+END
+	sed -i \
+		-e '/^BRANCH :=/ {G;a # XtraDeb workaround' \
+		-e 'a export PKG_CONFIG_PATH = $(CURDIR)/debian/pkgconfig' \
+		-e '}' \
+		$debian/rules
+fi
+
+##
+## Patch series modifications
+##
+
+new_patch xtradeb-gyp-subset.patch
+
+new_patch xtradeb-nodejs20-c-ares-downgrade.patch
+new_patch xtradeb-nodejs20-no-double-build.patch
+
 } # xd_convert()
+
+################################################################
+
+xd_check() {
+
+include_xtradeb_ppa=deps
+
+default_check "$@"
+
+} # xd_check()
 
 ################################################################
 
