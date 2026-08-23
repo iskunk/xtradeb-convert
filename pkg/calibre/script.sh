4699828a2fb4490403991fe19ac2bc7428037fd9
Daniel Richard G. <skunk@iSKUNK.ORG>
2026-08-23 00:02:38 -0400

    calibre: Update for 8.16 / 9.13
    Also, update patch names to conform to current XtraDeb conventions.
    
    0101-XtraDeb-drop-piper.patch: Refresh with a smaller context (-U2) to
    make it less fragile
    
    0103-XtraDeb-drop-pykakasi.patch: New patch to (messily) remove the
    pykakasi dependency from calibre 9.x.x for the benefit of resolute
    
    0104-XtraDeb-no-network-v{8,9}.patch: New patches to disable a test
    that fails in a localhost-only network environment
    
    calibre/script.sh:
    
    * Add a comment regarding version 7.x.x
    
    * Add logic for split support of 8.x.x and 9.x.x
    
    * Add a major_version variable for conditionalizing conversion logic
    
    * Apply build-dep downgrades only on noble/8.x.x, as they don't apply
      to resolute/9.x.x
    
    * Tweak sed(1) program formatting
    
    * Add logic to conditionally drop pykakasi
    
    * Enable parallelization of the CMake portion of the build
    
    * Quash a huge number of lintian unusual-interpreter warnings
    
    * Apply new patches

--- pkg/calibre/script.sh
+++ pkg/calibre/script.sh
@@ -8,27 +8,66 @@
 
 xd_convert() {
 
+# Too much missing to make this work, even for version 7.x.x
 ! ubuntu_dist jammy || not_supported
 
-ubuntu_dist noble || not_applicable
+if dpkg --compare-versions $deb_version ge 9.0.0
+then
+	# Can't build 9.x.x on noble; it really needs Python 3.14
+	! ubuntu_dist noble || not_supported
+#
+elif dpkg --compare-versions $deb_version ge 8.0.0
+then
+	ubuntu_dist noble VENDOR \
+	|| not_applicable 'please build version >= 9.x.x'
+else
+	error 'package version is too old for conversion'
+fi
 
-################################################################
-##
-## Modifications to allow building on Ubuntu noble
-##
-################################################################
+major_version=${deb_version%%.*}
 
-# Downgrade some dependencies to what's available
-sed -i -r \
-	-e '/^\s+dh-python /s/>= \S+\)/>= 6.2024)/' \
-	-e '/^\s+python3-py7zr /s/>= \S+\)/>= 0.11.3)/' \
-	$debian/control
+if ubuntu_dist noble
+then
+	# Downgrade some dependencies to what's available
+	sed -ri \
+		-e '/^\s+dh-python / s/>= \S+\)/>= 6.2024)/' \
+		-e '/^\s+python3-py7zr / s/>= \S+\)/>= 0.11.3)/' \
+		$debian/control
 
-# Drop some others that aren't available
-sed -i -r \
-	-e '/^\s+libonnxruntime-dev,/d' \
-	-e '/^\s+qt6-svg-plugins,/d' \
-	$debian/control
+	# Drop some others that aren't available
+	sed -ri \
+		-e '/^\s+libonnxruntime-dev,/ d' \
+		-e '/^\s+qt6-svg-plugins,/ d' \
+		$debian/control
+fi
+
+drop_pykakasi=false
+
+if ubuntu_dist noble resolute && \
+   grep -q '^\s*python3-pykakasi,' $debian/control
+then
+	# Not available before stonking
+	drop_pykakasi=true
+	sed -ri -e '/^\s+python3-pykakasi,/ d' $debian/control
+fi
+
+cat > $debian/xtradeb.tmp << 'END'
+
+# enable parallelization if requested
+ifneq (,$(filter parallel=%,$(DEB_BUILD_OPTIONS)))
+export CMAKE_BUILD_PARALLEL_LEVEL=$(patsubst parallel=%,%,$(filter parallel=%,$(DEB_BUILD_OPTIONS)))
+endif
+END
+(cd $debian && sed -i '/^export VERBOSE=1/ r xtradeb.tmp' rules)
+rm $debian/xtradeb.tmp
+
+# 900+ lintian warnings from these
+cat >> $debian/calibre.lintian-overrides << END
+
+# XtraDeb
+calibre: unusual-interpreter python [usr/lib/calibre/**/*.py]
+calibre: unusual-interpreter python [usr/share/calibre/default_tweaks.py]
+END
 
 ##
 ## Patch series modifications
@@ -41,11 +80,18 @@ then
 
 	# python3-pyzstd is not available before oracular
 	sed -i 's/python3-pyzstd,/python3-zstd,/' $debian/control
-	new_patch 0099-rewrite-test_zstd.patch
+	new_patch 0100-XtraDeb-rewrite-test_zstd.patch
 
-	new_patch 0100-drop-piper.patch
-	new_patch 0101-py7zr-compat.patch
+	new_patch 0101-XtraDeb-drop-piper.patch
+	new_patch 0102-XtraDeb-py7zr-compat.patch
 fi
+
+if $drop_pykakasi
+then
+	new_patch 0103-XtraDeb-drop-pykakasi.patch
+fi
+
+new_patch 0104-XtraDeb-no-network-v$major_version.patch
 
 } # xd_convert()
 
